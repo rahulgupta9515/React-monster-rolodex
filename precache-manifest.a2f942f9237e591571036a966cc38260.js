67e4ae15565ad17974b7bea16e8502b41f8b5b79
Gupta, Rahul [PS] <rahul.gupta@qainfotech.com>
2020-03-08 19:28:44 +0530

    Updates

---
+++ precache-manifest.a2f942f9237e591571036a966cc38260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfc0df740ef2a0b754fdd340e01baab",
+    "url": "/React-monster-rolodex/index.html"
+  },
+  {
+    "revision": "335a1556b9859c11863f",
+    "url": "/React-monster-rolodex/static/css/main.ff43a5fd.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/React-monster-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-monster-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335a1556b9859c11863f",
+    "url": "/React-monster-rolodex/static/js/main.f3172919.chunk.js"
+  },
+  {
+    "revision": "ee379a30f993bca4c0b0",
+    "url": "/React-monster-rolodex/static/js/runtime-main.295ee336.js"
+  }
+]);
